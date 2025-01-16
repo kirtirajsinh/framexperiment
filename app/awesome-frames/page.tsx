9bf661879e931f83bf35a121934a405ea54f9944
kirtirajsinh <kirtiraj3738@gmail.com>
2025-01-17 04:25:48 +0530

    Add new pages for Awesome Frames and Learn, and update global styles to hide scrollbars

---
+++ app/awesome-frames/page.tsx
@@ -0,0 +1,13 @@
+import Examples from "@/components/Examples/Examples";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold">Awesome Frames</h1>
+      <Examples />
+    </div>
+  );
+};
+
+export default page;
---
+++ app/learn/page.tsx
@@ -0,0 +1,8 @@
+import LearnScreen from "@/components/learn/LearnScreen";
+import React from "react";
+
+const page = () => {
+  return <LearnScreen />;
+};
+
+export default page;
